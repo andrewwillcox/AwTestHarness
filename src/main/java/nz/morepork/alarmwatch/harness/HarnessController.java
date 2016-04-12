deb16f4f3d7e74c5128f5753d707df6cba669a38
Colin <colin.prinn@sparkventures.co.nz>
2016-04-13 11:45:45 +1200

    Test for auto recompile

--- src/main/java/nz/morepork/alarmwatch/harness/HarnessController.java
+++ src/main/java/nz/morepork/alarmwatch/harness/HarnessController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HarnessController {
+class HarnessController {
     private static final Logger logger = LoggerFactory.getLogger(HarnessController.class);
 
     @RequestMapping(method = RequestMethod.POST, path = "Account/OffLineClient")
