4a6d02dc7c7a43cf6f18ee2e5688062367b7467a
Colin <colin.prinn@sparkventures.co.nz>
2016-04-13 13:07:41 +1200

    Removed spring boot as embedded tomcat was causing issues

--- src/main/java/nz/morepork/alarmwatch/harness/Application.java
+++
@@ -1,9 +0,0 @@
-package nz.morepork.alarmwatch.harness;
-
-import org.springframework.boot.SpringApplication;
-
-public class Application {
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(AppConfig.class, args);
-    }
-}
--- src/main/java/nz/morepork/alarmwatch/harness/HarnessController.java
+++ src/main/java/nz/morepork/alarmwatch/harness/HarnessController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping
 class HarnessController {
     private static final Logger logger = LoggerFactory.getLogger(HarnessController.class);
 
