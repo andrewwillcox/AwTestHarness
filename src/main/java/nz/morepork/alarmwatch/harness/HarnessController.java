927c9e1e069280f303231f2fa012e0d751b1421e
Andrew Willcox <andrew.willcox@sparkventures.co.nz>
2016-09-06 12:34:31 +1200

    Removing java 8 imports

--- src/main/java/nz/morepork/alarmwatch/harness/HarnessController.java
+++ src/main/java/nz/morepork/alarmwatch/harness/HarnessController.java
@@ -15,8 +15,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 @EnableWebMvc
 @RestController
